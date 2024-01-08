6615a645b9e96081a5ce8b4b702cedde80d3c897
vsheva <valera.sheva@gmail.com>
2024-01-07 21:22:33 -0500

    get request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,35 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    //console.log(postData);
+    this.http.post(
+      "https://ng-http-7339f-default-rtdb.firebaseio.com/posts.json",
+      postData
+    ).subscribe((responseData)=>{
+      console.log(responseData);
+    })
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+
+  private fetchPosts() {
+    this.http.get("https://ng-http-7339f-default-rtdb.firebaseio.com/posts.json")
+      .subscribe(posts =>{
+        console.log(posts);
+      })
   }
 }
