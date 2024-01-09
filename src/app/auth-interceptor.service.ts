c4270b8aedc78cb809998c6d3787a427c70284b0
vsheva <valera.sheva@gmail.com>
2024-01-09 00:13:56 -0500

    complete task

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,8 +1,22 @@
-import {HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {HttpEventType, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {tap} from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor {
 intercept(req: HttpRequest<any>, next:HttpHandler) {
+/*
   console.log("Request is on its way");
-  return next.handle(req) // we let it continue - request leave the app
+  console.log(req.url);
+*/
+
+  const modofiedRequest = req.clone({headers: req.headers.append("Auth", "abc")});
+  return next.handle(modofiedRequest)/*.pipe(tap((event)=>{
+    console.log(event);
+    if(event.type===HttpEventType.Response) {
+      console.log("Response arrived, body data: ");
+      console.log(event.body);
+    }
+  }));*/
+
+  // we let it continue - request leave the app
 }
 }
